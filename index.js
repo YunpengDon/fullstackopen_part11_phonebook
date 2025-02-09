be785f562ade4a81b811c06eaa753f7c99ff0146
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2025-02-09 22:33:53 +0800

    add health check route

--- index.js
+++ index.js
@@ -15,29 +15,10 @@ morgan.token('body', function (req, ) { return JSON.stringify(req.body) })
 // Configure to log messages to console based on the tiny configuration.
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-// let phonebook = [
-//   {
-//     'id': 1,
-//     'name': 'Arto Hellas',
-//     'number': '040-123456'
-//   },
-//   {
-//     'id': 2,
-//     'name': 'Ada Lovelace',
-//     'number': '39-44-5323523'
-//   },
-//   {
-//     'id': 3,
-//     'name': 'Dan Abramov',
-//     'number': '12-43-234345'
-//   },
-//   {
-//     'id': 4,
-//     'name': 'Mary Poppendieck',
-//     'number': '39-23-6423122'
-//   }
-// ]
+// Helth check for deployment
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(people => {
